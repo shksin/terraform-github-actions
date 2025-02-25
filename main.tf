b242b3b48ef1e8ca51493f311c89652d108fb31e
shksin <deepshikha16@gmail.com>
2025-02-25 20:08:18 +1100

    create virtual network

--- main.tf
+++ main.tf
@@ -3,3 +3,17 @@ resource "azurerm_resource_group" "rg-terraform-github-actions-demo" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-terraform-github-actions-demo.name
+}
+
+resource "azurerm_subnet" "default" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg-terraform-github-actions-demo.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
