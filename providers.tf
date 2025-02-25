e59680bc8111ee857085555ac3a2bc30051e3f45
shksin <deepshikha16@gmail.com>
2025-02-25 17:22:29 +1100

    update provider settings

--- providers.tf
+++ providers.tf
@@ -2,14 +2,21 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = ">= 3.7.0"
     }
-    azuread = {
-      source = "hashicorp/azuread"
-    }
+  }
+
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-github-actions-state"
+    storage_account_name = "terraformgithubactions"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
